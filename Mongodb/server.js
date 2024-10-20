c431b7c87acf2a214ea931815e5a922ea3223398
vashishtkumar <vashishtk270@gmail.com>
2024-10-20 21:34:46 +0530

    complete CRUD OPERATION

--- Mongodb/server.js
+++ Mongodb/server.js
@@ -1,6 +1,7 @@
  const express=require("express");
  const mongodb=require("mongodb");
  const app=express();
+ const objid=mongodb.ObjectId;
 
  app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -25,7 +26,6 @@ console.log("unable to connect database",err);
     dinstance.collection("student").find({ }).toArray().then((res1)=>{
        res.render("home",{data:res1});
     })
-
  })
 
  app.get("/add",(req,res)=>{
@@ -56,7 +56,37 @@ app.post("/update",(req,res)=>{
     dinstance.collection("student").updateMany({name:req.body.updateName},{$set:{"name":req.body.newName}})
     res.redirect("/");
 })
- app.listen(3000,(err)=>{
+
+
+app.get("/viewData/:id",(req,res)=>{
+dinstance.collection("student").findOne({_id:new objid(req.params.id)}).then((res1)=>{
+console.log(res1);
+res.render("view",{data:res1});
+})
+})
+
+app.get("/editData/:id",(req,res)=>{
+    dinstance.collection("student").findOne({_id:new objid(req.params.id)}).then((res1)=>{
+    res.render("updateAll",{data:res1});
+    })
+})
+
+app.post("/edit",(req,res)=>{
+    dinstance.collection("student").updateOne({_id:new objid(req.body.id)},{$set:{name:req.body.name,age:req.body.age}})
+    res.redirect("/");
+})
+
+app.get("/deleteData/:id",(req,res)=>{
+    dinstance.collection("student").findOne({_id:new objid(req.params.id)}).then((res1)=>{
+        res.render("deleteAll",{data:res1});
+    })
+})
+
+app.get("/deleteP/:id",(req,res)=>{
+    dinstance.collection("student").deleteOne({_id:new objid(req.params.id)});
+    res.redirect("/");
+})
+app.listen(3000,(err)=>{
     console.log("connected at port 3000");
  })
 
